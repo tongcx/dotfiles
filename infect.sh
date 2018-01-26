0bace21ffacf8d7d3d5ecb4187f6632d98456c55
Chaoxu Tong <chaoxu@chaoxu-c02w10dlhtdg.corp.uber.com>
2018-01-25 16:40:09 -0800

    update infect

--- infect.sh
+++ infect.sh
@@ -1,9 +1,9 @@
-folder=snippets/dotfiles
+folder=~/dotfiles
 
 cd
-echo source ~/$folder/bash_profile >> ~/.bash_profile
-ln -sv $folder/tmux.conf .tmux.conf
-ln -sv $folder/vimrc .vimrc
+echo source $folder/bash_profile >> ~/.bash_profile
+ln -sv $folder/tmux.conf ~/.tmux.conf
+ln -sv $folder/vimrc ~/.vimrc
 
 brew install reattach-to-user-namespace
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
